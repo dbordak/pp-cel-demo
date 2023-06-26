cbfc3144e190dd92a5274b22967b6c044449fa30
Daniel Bordak <dbordak@fastmail.fm>
2023-06-25 20:24:08 -0400

    Example from project description

--- main.go
+++ main.go
@@ -8,19 +8,25 @@ import (
 )
 
 func main() {
-	env, err := cel.NewEnv(cel.Variable("name", cel.StringType))
-	// Check err for environment setup errors.
+	env, err := cel.NewEnv(cel.Variable("user", cel.MapType(cel.StringType, cel.AnyType)))
 	if err != nil {
 		log.Fatalf("failed initializing CEL: %s", err)
 	}
-	ast, iss := env.Compile(`"Hello world! I'm " + name + "."`)
+
+	example := `user.balance >= 500.0 && user.gender == "female" && user.age <= 30 && user.id == "yf2"`
+	ast, iss := env.Compile(example)
 	// Check iss for compilation errors.
 	if iss.Err() != nil {
 		log.Fatalln(iss.Err())
 	}
 	prg, err := env.Program(ast)
 	out, _, err := prg.Eval(map[string]interface{}{
-		"name": "CEL",
+		"user": map[string]interface{}{
+			"userId":         "yn1",
+			"gender":         "male",
+			"age":            10,
+			"accountBalance": 500,
+		},
 	})
 	fmt.Println(out)
 
