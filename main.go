1c7adb0ce9107c9be9e3f0ea08671008887bffcd
Daniel Bordak <dbordak@fastmail.fm>
2023-06-25 20:38:09 -0400

    Broke example out into function

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
+	"reflect"
 
 	"github.com/google/cel-go/cel"
+	"github.com/google/cel-go/common/types"
 )
 
 func main() {
@@ -12,22 +15,43 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed initializing CEL: %s", err)
 	}
+	user := map[string]interface{}{
+		"userId":         "yn1",
+		"gender":         "male",
+		"age":            10,
+		"accountBalance": 500,
+	}
 
+	out, err := example(env, user)
+	if err != nil {
+		log.Fatalf("failed evaluating example statement: %s", err)
+	}
+	fmt.Println(out)
+}
+
+func example(env *cel.Env, user map[string]interface{}) (bool, error) {
 	example := `user.balance >= 500.0 && user.gender == "female" && user.age <= 30 && user.id == "yf2"`
 	ast, iss := env.Compile(example)
-	// Check iss for compilation errors.
-	if iss.Err() != nil {
-		log.Fatalln(iss.Err())
-	}
-	prg, err := env.Program(ast)
-	out, _, err := prg.Eval(map[string]interface{}{
-		"user": map[string]interface{}{
-			"userId":         "yn1",
-			"gender":         "male",
-			"age":            10,
-			"accountBalance": 500,
-		},
-	})
-	fmt.Println(out)
 
+	if iss.Err() != nil {
+		return false, iss.Err()
+	}
+
+	prg, err := env.Program(ast)
+	if err != nil {
+		return false, err
+	}
+	out, _, err := prg.Eval(map[string]interface{}{
+		"user": user,
+	})
+	if err != nil {
+		return false, err
+	}
+
+	if !types.IsBool(out) {
+		return false, errors.New("statements must evaluate to a boolean")
+	}
+	refType := reflect.TypeOf(true)
+	val, err := out.ConvertToNative(refType)
+	return val.(bool), err
 }
